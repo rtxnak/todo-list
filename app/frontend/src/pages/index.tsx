650edb8558172ae1d181a48d2e044cb923a82a40
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-07-03 14:57:08 -0300

    feat: installed dependencies and first configuration

---
+++ app/frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      TODO LIST
+    </div>
+  )
+}
