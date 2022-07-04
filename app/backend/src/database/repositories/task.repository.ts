01c6e26fbce05f081705339cdc28ab1738c186c8
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-07-04 17:55:30 -0300

    feat: added repository for method POST on path '/' with function create

--- app/backend/src/database/repositories/task.repository.ts
+++ app/backend/src/database/repositories/task.repository.ts
@@ -1,9 +1,15 @@
+
 import { ITask } from "../../interfaces/ITask.interface";
 import Task from "../models/Tasks";
 
 export default class TaskRepository {
   findAll = async (): Promise<ITask[]> => {
     const result = await Task.findAll();
+    return result;
+  };
+
+  create = async (task: any): Promise<ITask> => {
+    const result = await Task.create(task);
     return result;
   };
 }
--- app/backend/src/interfaces/ITaskRespository.interface.ts
+++ app/backend/src/interfaces/ITaskRespository.interface.ts
@@ -1,5 +1,6 @@
 import { ITask } from "./ITask.interface"
 
 export interface ITaskRepository {
+  create(task: ITask): Promise<ITask>
   findAll(): Promise<ITask[]>
 }
