ad0491186dcd1cc591cece3a2b8342d1b8461d55
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-07-04 16:10:57 -0300

    feat: added database config., tasks table migration and sequelizerc paths

---
+++ app/backend/src/database/migrations/20220704163915-tasks.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("tasks", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable("tasks");
+  },
+};
