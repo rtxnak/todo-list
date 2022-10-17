0262d9f297823545450c6d32affa4fa0719f3964
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-10-17 19:42:51 -0300

    test: created tests for ENDPOINT POST on path '/'

--- app/backend/src/tests/task.test.ts
+++ app/backend/src/tests/task.test.ts
@@ -6,6 +6,7 @@ import Task from '../database/models/Tasks';
 import { Response } from 'superagent';
 
 import taskModelMock from './mocks/TaskModel.mock';
+import newTaskModelMock from './mocks/NewTaskModel.mock';
 
 chai.use(chaiHttp);
 
@@ -29,6 +30,34 @@ describe('todo list API test', () => {
     });
     it('return a array with all tasks', () => {
       expect(chaiHttpResponse.body).to.be.deep.eq(taskModelMock);
+    });
+  });
+
+  describe('POST on path "/", SUCCESS creating new task and correct status code', () => {
+    before(async () => {
+      sinon.stub(Task, 'create').resolves(newTaskModelMock as Task)
+      chaiHttpResponse = await request(app).post('/')
+        .send({
+          description: "fifth task",
+          status: "ongoing",
+          date: "17/10/2022",
+        });
+    });
+
+    after(async () => {
+      (Task.create as sinon.SinonStub).restore();
+    })
+
+    it('return status code "201"', () => {
+      expect(chaiHttpResponse).to.have.status(201);
+    });
+    it('return the new task with id', () => {
+      expect(chaiHttpResponse.body).to.have.property('id');
+    });
+    it('return the new task with correct description, date and pending status', () => {
+      expect(chaiHttpResponse.body.description).to.be.equal('fifth task');
+      expect(chaiHttpResponse.body.status).to.be.equal('pending');
+      expect(chaiHttpResponse.body.date).to.be.equal('17/10/2022');
     });
   });
 });
