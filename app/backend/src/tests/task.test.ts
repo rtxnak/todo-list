e4643b952f230951eca82a3d7e7271ae15cbe4b9
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-10-17 19:42:08 -0300

    test: created tests for ENDPOINT GET on path '/'

---
+++ app/backend/src/tests/task.test.ts
@@ -0,0 +1,35 @@
+import sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { app } from '../app';
+import Task from '../database/models/Tasks';
+import { Response } from 'superagent';
+
+import taskModelMock from './mocks/TaskModel.mock';
+
+chai.use(chaiHttp);
+
+const { expect, request } = chai;
+
+describe('todo list API test', () => {
+  let chaiHttpResponse: Response;
+
+  describe('GET on path "/", SUCCESS in returning task list and status code', () => {
+    before(async () => {
+      sinon.stub(Task, 'findAll').resolves(taskModelMock as Task[])
+      chaiHttpResponse = await request(app).get('/');
+    });
+
+    after(() => {
+      (Task.findAll as sinon.SinonStub).restore();
+    })
+
+    it('return status code "200"', () => {
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+    it('return a array with all tasks', () => {
+      expect(chaiHttpResponse.body).to.be.deep.eq(taskModelMock);
+    });
+  });
+});
+
